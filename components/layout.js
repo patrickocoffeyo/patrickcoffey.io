33f8d38d913d0786edfa4a5443abd962737fb52d
Patrick Coffey <patrickcoffey48@gmail.com>
2021-07-07 21:30:41 -0500

    feat: basic customization, as well as typescript configuration

--- components/layout.js
+++ components/layout.js
@@ -4,8 +4,8 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = '[Your Name]'
-export const siteTitle = 'Next.js Sample Website'
+const name = 'Patrick Coffey'
+export const siteTitle = 'Patrick Coffey'
 
 export default function Layout({ children, home }) {
   return (
@@ -14,7 +14,7 @@ export default function Layout({ children, home }) {
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
-          content="Learn how to build a personal website using Next.js"
+          content="I am a senior software engineer based in Austin TX, currently working remotely as an engineer at Four Kitchens, where I build and maintain large-scale Node.js-based applications for NBC."
         />
         <meta
           property="og:image"
@@ -23,7 +23,6 @@ export default function Layout({ children, home }) {
           )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
         />
         <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <header className={styles.header}>
         {home ? (
